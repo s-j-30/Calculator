636d210447a86e4a7721778283bddac88b8cbe21
Sandeep <sandeepjain30@gmail.com>
2021-01-24 16:15:33 +0000

    fixed linting

--- calculator.py
+++ calculator.py
@@ -4,11 +4,14 @@ basic calculator functions
 """
 import math
 
+
 def add(x, y):
-    return x+y
+    return x + y
+
 
 def multiply(*args):
     return math.prod(args)
 
+
 def subtract(x, y):
-    return x-y
+    return x - y
