e85c512194adc13fc48ae59dcff6155bc29a6b5b
Sandeep <sandeepjain30@gmail.com>
2021-01-24 16:18:50 +0000

    linting of test file

--- test_calculator.py
+++ test_calculator.py
@@ -4,18 +4,14 @@ test calculator
 """
 from calculator import add, subtract, multiply
 
+
 class TestCalculator:
 
-
     def test_add(self):
-        assert 10 == add(4,6)
-
+        assert 10 == add(4, 6)
 
     def test_subtract(self):
-        assert 2 == subtract(6,4)
-
+        assert 2 == subtract(6, 4)
 
     def test_multiply(self):
-        assert 24 == multiply(2,3,4)
-        
-
+        assert 24 == multiply(2, 3, 4)
