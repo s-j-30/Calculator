c4e00f9c10a18fbecc5e99fb69c7a66d5ca7ca9b
Sandeep <sandeepjain30@gmail.com>
2021-01-24 15:04:16 +0000

    calculator for add, subtract and multiply

---
+++ calculator.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+basic calculator functions
+"""
+import math
+
+def add(x, y):
+    return x+y
+
+def multiply(*args):
+    return math.prod(args)
+
+def subtract(x, y):
+    return x-y
