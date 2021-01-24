171adab069d6704754c32196274c1dabfa7b8286
Sandeep <sandeepjain30@gmail.com>
2021-01-24 15:44:32 +0000

    added tests

---
+++ test_calculator.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+test calculator
+"""
+from calculator import add, subtract, multiply
+
+class TestCalculator:
+
+
+    def test_add(self):
+        assert 10 == add(4,6)
+
+
+    def test_subtract(self):
+        assert 2 == subtract(6,4)
+
+
+    def test_multiply(self):
+        assert 24 == multiply(2,3,4)
+        
+
